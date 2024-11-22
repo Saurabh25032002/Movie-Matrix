ebfb39cb4a0435673719cfdcc24f0da29d9be191
Saurabh25032002 <sarahpanwar992@gmail.com>
2024-11-22 19:18:43 +0530

    added https url

--- style.js
+++ style.js
@@ -10,7 +10,7 @@ const inputBox=document.querySelector('.inputBox');
 
     try{
         const apikeyy="38311a73";
-const url=`http://www.omdbapi.com/?apikey=${apikeyy}&t=${movie}`;
+const url=`https://www.omdbapi.com/?apikey=${apikeyy}&t=${movie}`;
 const response= await fetch(url);
 
 if(!response.ok)
